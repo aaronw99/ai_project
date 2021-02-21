9bc5b31bd08fc9bd4e436e0a0008310c7a528038
Aaron Wong <aaron.t.wong@vanderbilt.edu>
2021-02-21 17:13:12 -0600

    update input handling, added gitignore

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-import csv
+import pandas as pd
 
-# Load resource weights
-resources = []
-with open('resources.csv', 'r') as resources_file:
-  csvreader = csv.reader(resources_file)
-  next(csvreader)
-  for row in csvreader:
-    resource = {}
-    resource['name'] = row[0]
-    resource['weight'] = row[1]
-    resources.append(resource)
-
-# For testing
-for resource in resources:
-  print(resource)
-
-# Load initial state of world
-state = []
-with open('initial_state.csv', 'r') as initial_state_file:
-  csvreader = csv.reader(initial_state_file)
-  next(csvreader)
-  for row in csvreader:
-    country = {}
-    country['name'] = row[0]
-    resource_num = 1
-    for col in row[1:]:
-      country['R' + str(resource_num)] = col
-      resource_num = resource_num + 1
-    state.append(country)
-
-# For testing
-for country in state:
-  print(country)
+resources_df = pd.read_excel('/Users/aaronwong/Desktop/code/ai_project/resources.xlsx')
+state_df = pd.read_excel('/Users/aaronwong/Desktop/code/ai_project/initial_state.xlsx')
+# print(resources_df)
+# print(state_df)
