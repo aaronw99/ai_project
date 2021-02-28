a7e969254e638b1732c3675f09456d7c1ec33947
unknown <yuxuan.zhang@vanderbilt.edu>
2021-02-27 19:47:40 -0600

    transfer

--- transfer.py
+++ transfer.py
@@ -1,35 +1,33 @@
 import copy
 
 class Transfer:
-    def __init__(self, state_1, state_2, country_1, country_2, template, multiplier = 1):
+    def __init__(self, state, country_1, country_2, trade):
         #country1 would be the main actor of trade here, so "in" and "out" corresponds 
         #to the resource of country1 and will be the contrary for country 2
-        self.state_1 = copy.deepcopy(state_1)
-        self.state_2 = copy.deepcopy(state_2)
+        self.state = copy.deepcopy(state)
         self.country_1 = country_1
         self.country_2 = country_2
-        self.template = template
-        self.multiplier = multiplier
+        self.trade = trade
     
     def execute(self):
-        inputs = self.template["in"]
-        outputs = self.template["out"]
+        inputs = self.trade["in"]
+        outputs = self.trade["out"]
         for r_type in inputs:
-            self.state_1[self.country_1][r_type] = self.state_1[self.country_1][r_type] + outputs[r_type] * self.multiplier
-            self.state_2[self.country_2][r_type] = self.state_2[self.country_2][r_type] - inputs[r_type] * self.multiplier
+            self.state[self.country_1][r_type] = self.state[self.country_1][r_type] + outputs[r_type] 
+            self.state[self.country_2][r_type] = self.state[self.country_2][r_type] - inputs[r_type] 
         for r_type in outputs:
-            self.state_1[self.country_1][r_type] = self.state_1[self.country_1][r_type] - outputs[r_type] * self.multiplier
-            self.state_2[self.country_2][r_type] = self.state_2[self.country_2][r_type] + inputs[r_type] * self.multiplier
-        return self.state_1, self.state_2
+            self.state[self.country_1][r_type] = self.state[self.country_1][r_type] - outputs[r_type]
+            self.state[self.country_2][r_type] = self.state[self.country_2][r_type] + inputs[r_type] 
+        return self.state
         
     def toString(self):
         string = "(TRANSFER " + self.country_1 
-        inputs = self.template["in"]
-        outputs = self.template["out"]
+        inputs = self.trade["in"]
+        outputs = self.trade["out"]
         for r_type in inputs:
-            string = string + "(" + r_type + " " + str(inputs[r_type] * self.multiplier) + ")"
+            string = string + "(" + r_type + " " + str(inputs[r_type]) + ")"
         string = string + " TRANSFER " + self.country_2 
         for r_type in outputs:
-            string = string + "(" + r_type + " " + str(outputs[r_type] * self.multiplier)+ ")"
+            string = string + "(" + r_type + " " + str(outputs[r_type])+ ")"
         string = string + "))"
         return string
