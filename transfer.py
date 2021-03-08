9280c5420cfbebc309961477daf84bb7dc54627f
Joshua Li <josh@MacBook-Pro.local>
2021-03-08 13:48:04 -0600

    added untradeable resources, organizational changes

--- transfer.py
+++ transfer.py
@@ -1,27 +1,29 @@
 import copy
 
+
 class Transfer:
-    def __init__(self, state, country_1, country_2, trade):
+    def __init__(self, state, country_from, country_to, trade):
         self.state = copy.deepcopy(state)
-        self.country_1 = country_1
-        self.country_2 = country_2
+        self.country_from = country_from
+        self.country_to = country_to
         self.trade = trade
-    
+
     def execute(self):
-        type = self.trade[0]
+        resource = self.trade[0]
         quantity = self.trade[1]
-        self.state[self.country_1][type] = self.state[self.country_1][type] - quantity 
-        self.state[self.country_2][type] = self.state[self.country_2][type] + quantity
+        self.state[self.country_from][resource] = self.state[self.country_from][resource] - quantity
+        self.state[self.country_to][resource] = self.state[self.country_to][resource] + quantity
         return self.state
-        
+
     def toString(self):
-        type = self.trade[0]
+        resource = self.trade[0]
         quantity = self.trade[1]
-        string = "(TRANSFER " + self.country_1 + " " + self.country_2 + " (" + type + " " + str(quantity) + "))"
+        string = "(TRANSFER FROM " + self.country_from + " TO " + \
+            self.country_to + " (" + resource + " " + str(quantity) + "))"
         return string
-    
+
     def isFrom(self):
-        return self.country_1
-    
+        return self.country_from
+
     def isTo(self):
-        return self.country_2
+        return self.country_to
