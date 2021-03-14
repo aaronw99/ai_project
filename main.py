ef67e56f1dcea9a8d932e5cd2e40f9a81eabe53b
Joshua Li <josh@MacBook-Pro.local>
2021-03-14 07:59:11 -0500

    added results for states 3-9

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import time
 
 # top level function as defined in the course website
 # note: we added one extra variable multiplier to expriment with failure cost
+
+
 def my_country_scheduler(your_country_name,
                          resources_filename,
                          initial_state_filename,
@@ -31,8 +33,10 @@ def my_country_scheduler(your_country_name,
 
 
 myCountry = "Atlantis"
+
+
 # change this value to run on different states
-initialStateNum = "2"
+initialStateNum = "3"
 initialStatePath = "test_initial_states/initial_state_" + initialStateNum + ".xlsx"
 resourceWeightPath = "resources.xlsx"
 output = "results/" + initialStateNum + "/schedules_"
@@ -41,30 +45,33 @@ numOutput = 10
 timeFile = open("results/" + initialStateNum + "/runtimes.txt", "a")
 # fix gamma = 0.95, x0 = 0, L = 1, k = 1, C_failure = -0.5 * discounted reward
 # expriment with different depths and frontier sizes
-# search with depth = 5, width = 5 on initial state initialStateNum 
+# search with depth = 5, width = 5 on initial state initialStateNum
 startTime = time.time()
 my_country_scheduler(myCountry, resourceWeightPath,
                      initialStatePath, output + "_1.txt", numOutput, 5, 5, 0.5)
 endTime = time.time()
-timeFile.write("Run time for expriment 1: " + str(endTime - startTime) + " seconds")
+timeFile.write("Run time for expriment 1: " +
+               str(endTime - startTime) + " seconds")
 timeFile.write("\n")
 print("Time elapsed: " + str(endTime - startTime) + " seconds")
 
-# search with depth = 5, width = 10 on initial state initialStateNum 
+# search with depth = 5, width = 10 on initial state initialStateNum
 startTime = time.time()
 my_country_scheduler(myCountry, resourceWeightPath,
                      initialStatePath, output + "_2.txt", numOutput, 5, 10, 0.5)
 endTime = time.time()
-timeFile.write("Run time for expriment 1: " + str(endTime - startTime) + " seconds")
+timeFile.write("Run time for experiment 2: " +
+               str(endTime - startTime) + " seconds")
 timeFile.write("\n")
 print("Time elapsed: " + str(endTime - startTime) + " seconds")
 
-# search with depth = 10, width = 5 on initial state initialStateNum 
+# search with depth = 10, width = 5 on initial state initialStateNum
 startTime = time.time()
 my_country_scheduler(myCountry, resourceWeightPath,
                      initialStatePath, output + "_3.txt", numOutput, 10, 5, 0.5)
 endTime = time.time()
-timeFile.write("Run time for expriment 1: " + str(endTime - startTime) + " seconds")
+timeFile.write("Run time for experiment 3: " +
+               str(endTime - startTime) + " seconds")
 timeFile.write("\n")
 print("Time elapsed: " + str(endTime - startTime) + " seconds")
 
@@ -75,7 +82,8 @@ startTime = time.time()
 my_country_scheduler(myCountry, resourceWeightPath,
                      initialStatePath, output + "_4.txt", numOutput, 5, 5, 1)
 endTime = time.time()
-timeFile.write("Run time for expriment 1: " + str(endTime - startTime) + " seconds")
+timeFile.write("Run time for experiment 4: " +
+               str(endTime - startTime) + " seconds")
 timeFile.write("\n")
 print("Time elapsed: " + str(endTime - startTime) + " seconds")
 
@@ -84,9 +92,9 @@ startTime = time.time()
 my_country_scheduler(myCountry, resourceWeightPath,
                      initialStatePath, output + "_5.txt", numOutput, 5, 5, 0.1)
 endTime = time.time()
-timeFile.write("Run time for expriment 1: " + str(endTime - startTime) + " seconds")
+timeFile.write("Run time for experiment 5: " +
+               str(endTime - startTime) + " seconds")
 timeFile.write("\n")
 print("Time elapsed: " + str(endTime - startTime) + " seconds")
 
 timeFile.close()
-
