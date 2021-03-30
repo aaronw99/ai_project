f0c7e9e3fbbc66210f1934b0a55278be3c5ae4d3
Zhixiang Wang <alexw@Zhixiangs-MacBook-Air.local>
2021-03-30 02:09:00 -0400

    bug fix

--- market.py
+++ market.py
@@ -50,6 +50,7 @@ class Market:
         self.asks = {}
         self.buyers = {}
         self.sellers = {}
+        self.reserves = {}
         
     def submitLimitBuy(self, orders: dict, name: str):
         for ticker in orders.keys():
@@ -64,6 +65,12 @@ class Market:
                     limitBuyOrder.setExpiration(order["expiration"])
                 liquidity.append(limitBuyOrder)
                 liquidity.sort(reverse = True)
+                if name not in self.reserves:
+                    self.reserves[name] = {}
+                if "cash" not in self.reserves[name]:
+                    self.reserves[name] = 0
+                self.reserves[name]["cash"] = self.reserves[name]["cash"] + quantity * strike
+                self.world[name]["cash"] = self.world[name]["cash"] - quantity * strike
     
     def submitLimitSell(self, orders: dict, name: str):
         for ticker in orders.keys():
@@ -78,6 +85,12 @@ class Market:
                     limitSellOrder.setExpiration(order["expiration"])
                 liquidity.append(limitSellOrder)
                 liquidity.sort()
+                if name not in self.reserves:
+                    self.reserves[name] = {}
+                if ticker not in self.reserves[name]:
+                    self.reserves[name][ticker] = 0
+                self.reserves[name][ticker] = self.reserves[name][ticker] + quantity
+                self.world[name][ticker] = self.world[name][ticker] - quantity
     
     def submitMarketBuy(self, orders: dict, name: str):
         for ticker in orders.keys():
@@ -105,21 +118,27 @@ class Market:
                 fillQuantity = order.getQuantity()
                 liquidity = self.asks[ticker]
                 lastPrice = self.quotePrice(ticker)["buyPrice"]
-                while liquidity and fillQuantity > 0:
-                    quantity = liquidity[0].getQuantity()
-                    lastPrice = liquidity[0].getStrike()
-                    if fillQuantity > quantity:
-                        #todo: modify state
-                        fillQuantity = fillQuantity - quantity
+                while liquidity and fillQuantity > 0 and self.world[order.getName()]["cash"] >= lastPrice:
+                    bestOffer = liquidity[0]
+                    sellerName = bestOffer.getName()
+                    sellingQuantity = bestOffer.getQuantity()
+                    sellingPrice = bestOffer.getStrike()
+                    maxFillingQuantity = min(sellingQuantity, int(self.world[order.getName()]["cash"] / sellingPrice))
+                    #modify state
+                    self.world[order.getName()][ticker] = self.world[order.getName()][ticker] + maxFillingQuantity 
+                    self.world[order.getName()]["cash"] = self.world[order.getName()]["cash"] - maxFillingQuantity  * sellingPrice
+                    self.world[sellerName]["cash"] = self.world[sellerName]["cash"] + maxFillingQuantity  * sellingPrice
+                    self.reserves[sellerName][ticker] = self.reserves[sellerName][ticker] - maxFillingQuantity
+                    fillQuantity = fillQuantity - maxFillingQuantity
+                    liquidity.setQuantity(sellingQuantity - maxFillingQuantity)
+                    lastPrice = sellingPrice
+                    if not liquidity.getQuantity():
                         liquidity.pop(0)
-                    else:
-                        liquidity[0].setQuantity(quantity - fillQuantity)
-                        fillQuantity = 0
                 if fillQuantity != 0:
                     #note: for now, if a market buy order cannot be fully filled,
                     #      the rest will be converted to a limit buy order with
                     #      the last executed price being the strike
-                    limitBuyOrder = Order(fillQuantity, lastPrice, order.getName(), order.getExpiration())
+                    limitBuyOrder = Order(fillQuantity, order.getName(), lastPrice, order.getExpiration())
                     self.bids[ticker].append(limitBuyOrder)
                     self.bids[ticker].sort(reverse = True)
             
@@ -130,20 +149,26 @@ class Market:
                 liquidity = self.bids[ticker]
                 lastPrice = self.quotePrice(ticker)["sellPrice"]
                 while liquidity and fillQuantity > 0:
-                    quantity = liquidity[0].getQuantity()
-                    lastPrice = liquidity[0].getStrike()
-                    if fillQuantity > quantity:
-                        #todo: modify state
-                        fillQuantity = fillQuantity - quantity
+                    bestOffer = liquidity[0]
+                    buyerName = bestOffer.getName()
+                    buyingQuantity = bestOffer.getQuantity()
+                    buyingPrice = bestOffer.getStrike()
+                    maxFillingQuantity = min(buyingQuantity, fillQuantity)
+                    #modify state
+                    self.world[order.getName()][ticker] = self.world[order.getName()][ticker] - buyingQuantity
+                    self.world[order.getName()]["cash"] = self.world[order.getName()]["cash"] + buyingQuantity * buyingPrice
+                    self.world[buyerName][ticker] = self.world[buyerName][ticker] + maxFillingQuantity
+                    self.reserves[buyerName]["cash"] = self.reserves[buyerName]["cash"] - maxFillingQuantity * buyingPrice
+                    fillQuantity = fillQuantity - maxFillingQuantity
+                    liquidity.setQuantity(buyingQuantity - maxFillingQuantity)
+                    lastPrice = buyingPrice
+                    if not liquidity.getQuantity():
                         liquidity.pop(0)
-                    else:
-                        liquidity[0].setQuantity(quantity - fillQuantity)
-                        fillQuantity = 0
                 if fillQuantity != 0:
                     #note: for now, if a market sell order cannot be fully filled,
                     #      the rest will be converted to a limit sell order with
                     #      the last executed price being the strike
-                    limitSellOrder = Order(fillQuantity, lastPrice, order.getName(), order.getExpiration())
+                    limitSellOrder = Order(fillQuantity, order.getName(), lastPrice, order.getExpiration())
                     self.asks[ticker].append(limitSellOrder)
                     self.asks[ticker].sort()
                 
