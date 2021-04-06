f4257cb17f1bee267d3ea22989ccf5938beb1148
Aaron <aaron.t.wong@vanderbilt.edu>
2021-04-06 14:51:06 -0500

    update so multiple turns are taken

--- mine.py
+++ mine.py
@@ -2,58 +2,38 @@ import copy
 import numpy as np
 
 # The Mine class represents the mine operation
-# As difficulty of the mine increases, the potential payout is higher, but so does the potential for
-# an even smaller payout. 
+# As difficulty of the mine increases, the amount of turns to complete the action increases, but the potential payout increases
 class Mine:
     def __init__(self, state, country, resource, difficulty = 1):
         self.state = copy.deepcopy(state)
         self.country = country
         self.resource = resource
-        self.difficulty = difficulty
+        self.initial_difficulty = difficulty
+        self.turns_remaining = difficulty
     
     def execute(self):
-        upper_limit = 100
-        lower_skewed = 20
-        upper_skewed = 20
-        if self.difficulty == 1:
-            if self.resource == 'R2' or self.resource == 'R3':
-                upper_limit = 100
-                lower_skewed = 20
-                upper_skewed = 20
-            elif self.resource == 'R6':
-                upper_limit = 50
-                lower_skewed = 10
-                upper_skewed = 10
-        elif self.difficulty == 2:
-            if self.resource == 'R2' or self.resource == 'R3':
-                upper_limit = 150
-                lower_skewed = 10
-                upper_skewed = 20
-            elif self.resource == 'R6':
-                upper_limit = 100
-                lower_skewed = 5
-                upper_skewed = 10
-        elif self.difficulty == 3:
-            if self.resource == 'R2' or self.resource == 'R3':
-                upper_limit = 200
-                lower_skewed = 5
-                upper_skewed = 20
-            elif self.resource == 'R6':
-                upper_limit = 150
-                lower_skewed = 3
-                upper_skewed = 10
+        # There is no payout until the correct amount of turns passes
+        if self.turns_remaining < 0:
+            return self.state
+        elif self.turns_remaining > 0:
+            self.turns_remaining = self.turns_remaining - 1
+            return self.state
+        else:
+            self.turns_remaining = -1
+            upper_limit = 50 * self.initial_difficulty
+            skewed = 20
 
-        pers = np.arange(1,upper_limit,1)
-        # Make each of the first and last n elements 10x more likely 
-        # The mining country is likely to either get a big or small payout, not some amount in the middle
-        prob = [10.0]*lower_skewed + [1.0]*(len(pers)-(lower_skewed+upper_skewed)) + [10.0]*upper_skewed
-        # Normalising to 1.0
-        prob /= np.sum(prob)
-        payout = np.random.choice(pers, 1, p=prob)
-        self.state[self.country][self.resource] = self.state[self.country][self.resource] + payout.item(0)
-        return self.state
+            pers = np.arange(1,upper_limit,1)
+            # Make the last n elements 10x more likely 
+            # The mining country is likely to get a big payout, but chance is still involved and the country can get a lower payout
+            prob = [1.0]*(len(pers)-(skewed)) + [10.0]*skewed
+            # Normalising to 1.0
+            prob /= np.sum(prob)
+            payout = np.random.choice(pers, 1, p=prob)
+            self.state[self.country][self.resource] = self.state[self.country][self.resource] + payout.item(0)
+            return self.state
         
     def toString(self):
         string = "(MINE " + self.country + " RESOURCE (" + self.resource + ") "
-        string = string + "DIFFICULTY (" + str(self.difficulty) + "))"
+        string = string + "DIFFICULTY (" + str(self.initial_difficulty) + "))"
         return string
--- tests/mine_test.py
+++ tests/mine_test.py
@@ -9,12 +9,14 @@ country1 = "Atlantis"
 
 transform_templates = [housing, alloys, electronics, farms, factories]
 myCountry = "Atlantis"
-world = World(myCountry, transform_templates, '/Users/aaronwong/Desktop/code/ai_project/test_initial_states/initial_state_2.xlsx', 'resources.xlsx')
+world = World(myCountry, transform_templates, '/home/aaronwong/code/ai_project/test_initial_states/initial_state_2.xlsx', 'resources.xlsx')
 startState = world.getStartState()
 print(startState)
 startstate = world.getStartState
 
 mine = Mine(startState, country1, 'R6', difficulty=2)
 print(mine.toString())
+newstate = mine.execute()
+newstate = mine.execute()
 newstate = mine.execute()
 print(newstate)
