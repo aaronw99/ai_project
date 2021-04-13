5ea4a3b83a87bfb456cb5a05dc05aa04213c3e34
Zhixiang Wang <alexw@144-129-135-202.biz.spectrum.com>
2021-04-12 21:13:16 -0400

    minor fix

--- simulation.py
+++ simulation.py
@@ -1,88 +1,27 @@
 from manager import Manager
 from naive_player import NaivePlayer
+import utils
 
 #set up game manager
 gm = Manager()
 #the following is taken from test_initial_tests/initial_state_1.xlsx
+initialStateNum = "1"
+initialStatePath = "test_initial_states/initial_state_" + initialStateNum + ".xlsx"
 #construct player agents
-playerA = NaivePlayer("Atlantis")
-playerAState = {
-    "R1": 100, 
-    "R2": 30, 
-    "R3": 20, 
-    "R4": 120, 
-    "R5": 1000,
-    "R6": 5000, 
-    "R7": 6000, 
-    "R8": 660, 
-    "R9": 50, 
-    "R18": 30,
-    "R19": 8,
-    "R20": 200, 
-    "R21": 60, 
-    "R22": 20, 
-    "R1'": 0,
-    "R5'": 0,
-    "R6'": 0,
-    "R18'": 0,
-    "R19'": 0,
-    "R20'": 0,
-    "R21'": 0,
-    "R22'": 0,
-    "cash": 7000
-}
-playerB = NaivePlayer("Brobdingnag")
-playerBState = {
-    "R1": 100, 
-    "R2": 10, 
-    "R3": 15, 
-    "R4": 100, 
-    "R5": 300,
-    "R6": 400, 
-    "R7": 30, 
-    "R8": 300, 
-    "R9": 40, 
-    "R18": 15,
-    "R19": 4,
-    "R20": 111, 
-    "R21": 50, 
-    "R22": 5, 
-    "R1'": 0,
-    "R5'": 0,
-    "R6'": 0,
-    "R18'": 0,
-    "R19'": 0,
-    "R20'": 0,
-    "R21'": 0,
-    "R22'": 0,
-    "cash": 15000
-}
-playerC = NaivePlayer("Carpania")
-playerCState = {
-    "R1": 100, 
-    "R2": 15, 
-    "R3": 10, 
-    "R4": 50, 
-    "R5": 50,
-    "R6": 200, 
-    "R7": 20, 
-    "R8": 200, 
-    "R9": 20, 
-    "R18": 25,
-    "R19": 3,
-    "R20": 123, 
-    "R21": 20, 
-    "R22": 10, 
-    "R1'": 0,
-    "R5'": 0,
-    "R6'": 0,
-    "R18'": 0,
-    "R19'": 0,
-    "R20'": 0,
-    "R21'": 0,
-    "R22'": 0,
-    "cash": 20000
-}
+playerAName = "Atlantis"
+playerA = NaivePlayer(playerAName)
+playerAState = utils.getInitialState(initialStatePath, playerAName)
+playerAState["cash"] = 7000
+
+playerBName = "Brobdingnag"
+playerB = NaivePlayer(playerBName)
+playerBState = utils.getInitialState(initialStatePath, playerBName)
+playerBState["cash"] = 15000
+
+playerCName = "Carpania"
+playerC = NaivePlayer(playerCName)
+playerCState = utils.getInitialState(initialStatePath, playerCName)
+playerCState["cash"] = 20000
 
 #add players to the game manager
 gm.addPlayer(playerA, playerAState)
